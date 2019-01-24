78075c969d89284d07ac8f5c790881d88ba69f48
sarnava1 <konarsarnava@gmail.com>
2019-01-24 22:32:44 +0530

    added client side routing instead of server side routing by including Link and NavLink of react routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import {Route, BrowserRouter, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -31,12 +31,26 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-       404!!
+       404!!--<Link to='/'>Go Home</Link>
     </div>
+);
+
+
+const Header = () => (
+    <header>
+      <h1>Expensify app</h1>
+      <NavLink to='/' activeClassName="is-active" exact={true}>Dashboard</NavLink>
+      <NavLink to='/create' activeClassName="is-active">Create Expense</NavLink>
+      <NavLink to='/edit' activeClassName="is-active">Edit Expense</NavLink>
+      <NavLink to='/help' activeClassName="is-active">Help</NavLink>
+    </header>
+       
 );
 
 const routes = (
    <BrowserRouter>
+     <div>
+     <Header />
      <Switch>
        <Route path="/" component={ExpenseDashboardPage} exact={true} />
        <Route path="/edit" component={EditExpensePage} />
@@ -44,6 +58,7 @@ const routes = (
        <Route path="/help" component={HelpPage} />
        <Route component={NotFoundPage} />
      </Switch>
+     </div>
    </BrowserRouter>
 );
 
