af0c232710818f9ca189b3c75b4d5bd3ea593c35
sarnava1 <konarsarnava@gmail.com>
2019-01-27 20:14:08 +0530

    started creating the redux file for expensify app with reducers and combine reducers

--- src/playgrounds/redux-expensify.js
+++ src/playgrounds/redux-expensify.js
@@ -1,5 +1,42 @@
 import { createStore, combineReducers } from 'redux';
 
+//Expenses reducer
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state= expensesReducerDefaultState, action) => {
+     switch(action.type){
+         default:
+            return state;
+     }
+};
+
+//filters reducer
+
+const filtersReducerDefaultState = {
+    text :'',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filterReducer = (state= filtersReducerDefaultState, action ) => {
+   switch (action.type) {
+       default:
+         return state;
+   }
+};
+
+// store creation
+
+const store = createStore(
+   combineReducers({
+       expenses: expensesReducer,
+       filters: filterReducer
+   })
+);
+
+
 const demoState = {
     expenses : [{
         id : 'dkfjbkjdf',
