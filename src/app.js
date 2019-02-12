28e2f030649d1532fd8c89b554b6f6dc47587e8a
sarnava1 <konarsarnava@gmail.com>
2019-02-13 00:09:12 +0530

    learnt about react-redux and added the first component which uses it ExpenseList

--- src/app.js
+++ src/app.js
@@ -3,22 +3,30 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+//import expensesReducer from './reducers/expenses';
+import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import { format } from 'url';
 import getVisibleExpenses from './selectors/expenses';
-
 const store = configureStore();
 
-console.log(store.getState());
+//console.log(store.getState());
+
 store.dispatch(addExpense({description: 'Water bill'}));
 store.dispatch(addExpense({description: 'Gas bill'}));
 store.dispatch(setTextFilter('bill'));
 
 const state=store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
-const getVisibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+const jsx = (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
 
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render( jsx, document.getElementById('app'));
