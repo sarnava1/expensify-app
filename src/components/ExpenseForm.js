8f75634268fc5dabce520fae8515db5842d56bc0
sarnava1 <konarsarnava@gmail.com>
2019-02-16 15:20:44 +0530

    addded moment and react-dates to pick the createdAt date

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,15 @@
 import React from 'react';
- 
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
 export default class ExpenseForm extends React.Component {
     state = {
        description: '',
        note: '',
-       amount: ''
+       amount: '',
+       createdAt : moment(),
+       calendarFocused: false
     };
     onDescriptionChange = (e) => {
        const description= e.target.value;
@@ -22,6 +27,16 @@ export default class ExpenseForm extends React.Component {
             this.setState( () => ({ amount }) );
         }
     };
+
+    onDateChange = (createdAt) => {
+        if (createdAt) {
+          this.setState(() => ({ createdAt }));
+        }
+      };
+    
+    onFocusChange = ({ focused }) => {
+            this.setState(() => ({ calendarFocused: focused }));
+     };
 
     render( ) {
         return (
@@ -41,6 +56,15 @@ export default class ExpenseForm extends React.Component {
                     value= { this.state.amount }
                     onChange = { this.onAmountChange }
 
+                 />
+                 <SingleDatePicker 
+                   date={this.state.createdAt}
+                   onDateChange={this.onDateChange}
+                   focused={this.state.calendarFocused}
+                   onFocusChange={this.onFocusChange}
+                   numberOfMonths={1}
+                   isOutsideRange={() => false}
+               
                  />
                  <textarea placeholder="add a note for your expense(optional)"
                  value= { this.state.note }
